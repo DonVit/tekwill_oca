c94016fccec23c866f7e0ba7d3d49067a8b98d40
Vitalie Doni <vitalie.doni@gmail.com>
2018-04-24 20:23:53 +0300

    my first java project

---
+++ lesson3/src/MyFirstPackage/MyFirstClass.java
@@ -0,0 +1,14 @@
+package MyFirstPackage;
+
+public class MyFirstClass {
+
+    public static void main(String[] args) {
+        int total=sum(1, 2);
+        System.out.print("My First java application"+total);
+    }
+
+    public static int sum(int x, int y){
+        return x+y;
+    }
+
+}
